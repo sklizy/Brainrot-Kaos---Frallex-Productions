83b095b73c9a6a5247d6f67ba1b4e477a10ba667
sklizy <franeu0926@edu.lerum.se>
2026-05-13 13:25:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openRickRoll(){
 window.open(
-'https://archive.org/details/rick-roll-video',
+'https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Fstamsite-s-5timmar-session-leak-v0-6csiajl2wkef1.jpg%3Fwidth%3D1080%26crop%3Dsmart%26auto%3Dwebp%26s%3D966511543ca15f83ae8cf57ee51eea7f7c6521da',
 '_blank'
 );
 }
